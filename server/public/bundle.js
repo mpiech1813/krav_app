72c58ed34d81c2bb9add8f6b948f312c8f163fed
mpiech1813 <maciejpiech18@gmail.com>
2021-12-25 14:34:40 -0600

    added babel file

---
+++ server/public/bundle.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./client/index.js":
+/*!*************************!*\
+  !*** ./client/index.js ***!
+  \*************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /home/robak/Documents/programowanie/Krav_app/client/index.js: Support for the experimental syntax 'jsx' isn't currently enabled (7:17):\n\n\u001b[0m \u001b[90m 5 |\u001b[39m \u001b[36mconst\u001b[39m root \u001b[33m=\u001b[39m document\u001b[33m.\u001b[39mgetElementById(\u001b[32m\"root\"\u001b[39m)\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 6 |\u001b[39m\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 7 |\u001b[39m \u001b[33mReactDOM\u001b[39m\u001b[33m.\u001b[39mrender(\u001b[33m<\u001b[39m\u001b[33mApp\u001b[39m \u001b[33m/\u001b[39m\u001b[33m>\u001b[39m\u001b[33m,\u001b[39m root)\u001b[33m;\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m   |\u001b[39m                 \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 8 |\u001b[39m\u001b[0m\n\nAdd @babel/preset-react (https://git.io/JfeDR) to the 'presets' section of your Babel config to enable transformation.\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://git.io/vb4yA) to the 'plugins' section to enable parsing.\n    at Parser._raise (/home/robak/Documents/programowanie/Krav_app/node_modules/@babel/parser/lib/index.js:569:17)\n    at Parser.raiseWithData (/home/robak/Documents/programowanie/Krav_app/node_modules/@babel/parser/lib/index.js:562:17)\n    at Parser.expectOnePlugin (/home/robak/Documents/programowanie/Krav_app/node_modules/@babel/parser/lib/index.js:3626:18)\n    at Parser.parseExprAtom (/home/robak/Documents/programowanie/Krav_app/node_modules/@babel/parser/lib/index.js:12055:18)\n    at Parser.parseExprSubscripts (/home/robak/Documents/programowanie/Krav_app/node_modules/@babel/parser/lib/index.js:11654:23)\n    at Parser.parseUpdate (/home/robak/Documents/programowanie/Krav_app/node_modules/@babel/parser/lib/index.js:11634:21)\n    at Parser.parseMaybeUnary (/home/robak/Documents/programowanie/Krav_app/node_modules/@babel/parser/lib/index.js:11609:23)\n    at Parser.parseMaybeUnaryOrPrivate (/home/robak/Documents/programowanie/Krav_app/node_modules/@babel/parser/lib/index.js:11421:61)\n    at Parser.parseExprOps (/home/robak/Documents/programowanie/Krav_app/node_modules/@babel/parser/lib/index.js:11428:23)\n    at Parser.parseMaybeConditional (/home/robak/Documents/programowanie/Krav_app/node_modules/@babel/parser/lib/index.js:11398:23)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./client/index.js"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
