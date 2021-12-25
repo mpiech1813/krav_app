97d38b70edb06895d675497fa627d33225b41800
mpiech1813 <maciejpiech18@gmail.com>
2021-12-25 15:18:28 -0600

    getting app component to display

--- client/components/App.js
+++ client/components/App.js
@@ -5,6 +5,8 @@ class App extends Component {
     return (
       <div>
         <h1>Hello from the component</h1>
+        <h2>Hello from h2</h2>
+        <h3>Hello from h3</h3>
       </div>
     );
   }
