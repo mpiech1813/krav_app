3f47bf14697330b99b08bca4292c203c5ab604c5
mpiech1813 <maciejpiech18@gmail.com>
2022-01-02 12:08:22 -0600

    created sync and seed

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,43 @@
 const db = require("./db");
-const Admin = require('./models/Admin')
-const Student = require('./models/Student')
+const Admin = require("./models/Admin");
+const Student = require("./models/Student");
 
 const syncAndSeed = async () => {
   try {
-      await db.sync({force:true})
+    await db.sync({ force: true });
 
-      const kpiech
+    const kpiech = await Admin.create({
+      userName: "kpiech",
+      password: "Robactwo18!",
+    });
+
+    const student1 = await Student.create({
+      fName: "bob",
+      lName: "smith",
+      picture: "",
+      fShot: true,
+      sShot: true,
+      booster: true,
+    });
+    const student2 = await Student.create({
+      fName: "zach",
+      lName: "clark",
+      picture: "",
+      fShot: true,
+      sShot: true,
+      booster: false,
+    });
+    const student3 = await Student.create({
+      fName: "john",
+      lName: "robot",
+      picture: "",
+      fShot: false,
+      sShot: false,
+      booster: false,
+    });
   } catch (error) {
     console.log("error occured in sync and seed");
   }
 };
+
+module.exports = syncAndSeed;
