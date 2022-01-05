4f9a8e049017616bf53de1ab18580f9123604768
mpiech1813 <maciejpiech18@gmail.com>
2022-01-05 16:34:07 -0600

    added loginRouter path

--- server/api/loginRouter.js
+++ server/api/loginRouter.js
@@ -2,9 +2,12 @@ const loginRouter = require('express').Router()
 
 loginRouter.get('/', async(req, res, next)=>{
     try {
-        
+        //redirection to Google's login API
+        require.redirect()
     } catch (error) {
         console.log('error occured in google oauth link')
         next(error)
     }
 })
+
+module.exports = loginRouter
