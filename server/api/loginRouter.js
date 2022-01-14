d767c615f6b7cfc5639464b473292e1fb34e900b
mpiech1813 <maciejpiech18@gmail.com>
2022-01-14 12:41:37 -0600

    created object with connection params

--- server/api/loginRouter.js
+++ server/api/loginRouter.js
@@ -4,7 +4,15 @@ const loginRouter = require("express").Router();
 
 loginRouter.get("/", async (req, res, next) => {
   try {
-    res.redirect(`https://accounts.google.com/o/oauth2/v2/auth`);
+    const connectionParams = {
+      redirect_uri: "",
+      response_type: "",
+      scope: "",
+    };
+
+    res.redirect(
+      `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.client_id}`
+    );
   } catch (error) {
     console.log("error occured in google oauth link");
     next(error);
