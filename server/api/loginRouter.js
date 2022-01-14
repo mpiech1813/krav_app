19857c2e55943763a634712048432d85de5bd067
mpiech1813 <maciejpiech18@gmail.com>
2022-01-14 13:09:36 -0600

    working out a redirect bug

--- server/api/loginRouter.js
+++ server/api/loginRouter.js
@@ -5,13 +5,17 @@ const loginRouter = require("express").Router();
 loginRouter.get("/", async (req, res, next) => {
   try {
     const connectionParams = {
-      redirect_uri: "",
-      response_type: "",
-      scope: "",
+      redirect_uri: "http://localhost:1813/api/google/callback",
+      response_type: "token",
+      scope: "https%3A//www.googleapis.com/auth/drive.metadata.readonlyx",
     };
 
     res.redirect(
-      `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.client_id}`
+      `https://accounts.google.com/o/oauth2/v2/auth?
+      response_type=token&
+       client_id=${process.env.client_id}&
+       redirect_uri:${connectionParams.redirect_uri}&
+       scope:${connectionParams.scope}`
     );
   } catch (error) {
     console.log("error occured in google oauth link");
