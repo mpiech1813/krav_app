793847fac588927a19330c51f1705886c13c9579
mpiech1813 <maciejpiech18@gmail.com>
2022-01-14 12:46:43 -0600

    added token router

--- server/api/router.js
+++ server/api/router.js
@@ -4,10 +4,12 @@ const router = require("express").Router();
 const adminRouter = require("./adminRouter");
 const studentRouter = require("./studentsRouter");
 const loginRouter = require("./loginRouter");
+const accessTokenROuter = require("./accessTokenRouter");
 
 // make the connections
 router.use("/admin", adminRouter);
 router.use("/student", studentRouter);
 router.use("/login", loginRouter);
+router.use("/google/callback", accessTokenROuter);
 
 module.exports = router;
